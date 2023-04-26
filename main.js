b3b7f868e7f9ac0870fd20db1aebac6cc4493519
Pear104 <schoolpear14@gmail.com>
2023-04-26 21:01:05 +0700

    maybe the last commit

--- main.js
+++ main.js
@@ -16,8 +16,8 @@ const data = {
         "content": "这是一段，我写这段是为了测试这个页面的更改语言功能。 这段很短。 这一段我在努力加长，hihi(～￣▽￣)～，应该是三行才算是通俗的段子。 终于可以加长了，非常感谢你看完这些废话"
     },
     tretrau: {
-        "title": " ảo thật đấy",
-        "content": "Zảo chang hảo Trung gúa Xiên chai Gua zẩu PING CHILLING gúa hấn xí goàn Pính Chilling Ping sư xú yu đu dính gì bi PING Chilling, sư xú yu đu dính gì, sư xú yu đu dính gì gô zui xì hấn, sồ gi xin zài Suǒ yǐ xiàn zài shì yīn yuè shí jiān Zhǔn bèi Yī, èr, sān Liǎng gè lǐ bài yǐ hòu Sù dù yǔ jī qíng jiǔ Liǎng gè lǐ bài yǐ hòu Sù dù yǔ jī qíng jiǔ Liǎng gè lǐ bài yǐ hòu Sù dù yǔ jī qíng jiǔBù yào wàng jì, bù yào cuò gu Jì dé qù diàn yǐng yuàn kàn sù dù yǔ jī qíng jiǔ Dòng zuò fē cháng hǎo Chà bù duō yī yàng bing chilling Zài jiàn"
+        "title": "ảo thật đấy",
+        "content": "Zảo chang hảo Trung gúa Xiên chai Gua zẩu PING CHILLING gúa hấn xí goàn Pính Chilling Ping sư xú yu đu dính gì bi PING Chilling, sư xú yu đu dính gì, sư xú yu đu dính gì gô zui xì hấn, sồ gi xin zài Suǒ yǐ xiàn zài shì yīn yuè shí jiān Dùn bei I, ơ, sần Loằng ge li pai ý hâu Su đu du zi uýnh zâu Loằng ge li pai ý hâu Su đu du zi uýnh zâu Loằng ge li pai ý hâu Su đu du zi uýnh zâu Bù yào wàng jì, bù yào cuò gu Jì dé qù diàn yǐng yuàn kàn sù dù yǔ jī qíng jiǔ Dòng zuò fē cháng hǎo Chà bù duō yī yàng bing chilling Zài zen"
     },
 }
 
@@ -30,10 +30,24 @@ const cena = document.querySelector(".johncena")
 const audio = document.querySelector("audio")
 const khabanh = document.querySelector(".khabanh")
 
+let a = localStorage.getItem("currentLanguage") || languageControl.value
+title.innerText = data[a].title
+paragraph.innerText = data[a].content
+languageControl.value = a
+if (a == "tretrau") {
+    cena.style.display = "inline-block"
+    khabanh.style.display = "inline-block"
+    audio.play()
+} else {
+    cena.style.display = "none"
+    khabanh.style.display = "none"
+    audio.pause()
+}
+
 languageControl.addEventListener("change", (e) => {
     const a = languageControl.value
     localStorage.setItem("currentLanguage", `${a}`)
-    title.innerHTML = data[a].title
+    title.innerText = data[a].title
     paragraph.innerText = data[a].content
     if (a == "tretrau") {
         audio.currentTime = 0
@@ -46,20 +60,6 @@ languageControl.addEventListener("change", (e) => {
         audio.pause()
     }
 })
-
-let a = localStorage.getItem("currentLanguage")
-title.innerHTML = data[a].title
-paragraph.innerText = data[a].content
-languageControl.value = a
-if (a == "tretrau") {
-    cena.style.display = "inline-block"
-    khabanh.style.display = "inline-block"
-    audio.play()
-} else {
-    cena.style.display = "none"
-    khabanh.style.display = "none"
-    audio.pause()
-}
 
 const setDarkMode = () => {
     body.setAttribute("data-theme", "dark")
